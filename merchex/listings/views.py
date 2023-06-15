1a525c9ba0b12f0626cf62bd6cbad68828c8716e
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-06-15 09:06:55 +0200

    J'ai réussi à faire une chemin relatif vers mon templates et le fichier index.html avec BASE_DIR

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
 
-from django.http import HttpResponse
-from django.shortcuts import render
+def index(request):
+    return render(request, "index.html")
 
-def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+
 from listings import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("hello/", views.hello)
+    path('', views.index, name="index"),
+    path("admin/", admin.site.urls)
+
 ]
